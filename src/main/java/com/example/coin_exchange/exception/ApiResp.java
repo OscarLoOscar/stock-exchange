aabcc3af5ede2ca1d50741747adefcfcaad0ec9c
Lo Oscar <lokongkitoscar@gmail.com>
2025-02-27 22:58:31 +0800

    update

--- src/main/java/com/example/coin_exchange/exception/ApiResp.java
+++ src/main/java/com/example/coin_exchange/exception/ApiResp.java
@@ -1,9 +1,12 @@
 package com.example.coin_exchange.exception;
 
+import com.example.coin_exchange.exception.exceptionEnum.Syscode;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResp<T> {
@@ -13,6 +16,19 @@ public class ApiResp<T> {
 
   public static <T> ApiResponseBuilder<T> builder(){
     return new ApiResponseBuilder<>();
+  }
+
+  @Getter
+  public static class ApiResponseBuilder<T> {
+    private String syscode;
+    private String message;
+    private T data;
+
+    public ApiResponseBuilder<T>status(Syscode syscode){
+      this.syscode=syscode.getSyscode();
+      this.message=syscode.getMessage();
+      return this;
+    }
   }
 
 }
